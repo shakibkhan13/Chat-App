fcc530275d6ab74b1e5fc4a057aaccb54d7d737c
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2025-01-07 22:24:57 +0600

    create folder and other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
+import { Routes , Route } from 'react-router-dom'
+import Login from './pages/Login/Login'
+import Chat from './pages/Chat/Chat'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 
 function App() {
 
   return (
-    <h1>
-      shakib
-    </h1>
+    <>
+      <Routes>
+        <Route path='/' element={<Login/>} />
+        <Route path='/chat' element={<Chat/>} />
+        <Route path='/Profile' element={<ProfileUpdate/>} />
+      </Routes>
+    
+    </>
   )
 }
 
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+
+import  './login.css'
+
+const Login = () => {
+  return (
+    <div>
+      <h2>log in</h2>
+    </div>
+  )
+}
+
+export default Login
